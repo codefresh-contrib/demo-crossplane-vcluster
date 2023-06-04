933454e34ae2f49d29f2500bf536f7e2a193ed93
Ilia Medvedev <ilia.medvedev@codefresh.io>
2023-06-04 14:04:18 +0300

    adjust demo

--- scripts/init-k3d-demo-env.sh
+++ scripts/init-k3d-demo-env.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 MYDIR=$(dirname $0)
-k3d cluster delete cf-kubecon23-demo
-k3d cluster create cf-kubecon23-demo
-k3d kubeconfig get cf-kubecon23-demo > /tmp/k3d-cf-kubecon23-demo.config
-export KUBECONFIG=/tmp/k3d-cf-kubecon23-demo.config
+k3d cluster delete cf-crossplane-vcluster-demo
+k3d cluster create cf-crossplane-vcluster-demo
+k3d kubeconfig get cf-crossplane-vcluster-demo > /tmp/k3d-cf-crossplane-vcluster-demo.config
+export KUBECONFIG=/tmp/k3d-cf-crossplane-vcluster-demo.config
 helm repo add argo https://argoproj.github.io/argo-helm && helm repo update
-# https://github.com/cf-kubecon23-demo/cf-kubecon23-demo/issues/2121
 helm install --repo https://argoproj.github.io/argo-helm --create-namespace --namespace argocd argocd argo-cd --version 5.21.0  --set "configs.cm.application\.resourceTrackingMethod=annotation" --wait
 kubectl -n argocd apply -f $MYDIR/../argocd-applications
